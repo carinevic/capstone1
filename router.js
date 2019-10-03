96ced1bbad4ed87431d93719b820466b6a4d31e8
Carine Ngwekwe <carine@Carines-MacBook-Pro.local>
2019-10-03 15:28:42 -0500

    search

--- router.js
+++ router.js
@@ -3,26 +3,20 @@ const router = express.Router()
 const userController = require('./controller/userController')
 const postController = require('./controller/postController')
 
-//users related routes
+// user related routes
 router.get('/', userController.home)
 router.post('/register', userController.register)
-router.post('/login',userController.login)
-router.post('/logout',userController.logout)
+router.post('/login', userController.login)
+router.post('/logout', userController.logout)
 
-
-//profile related routes
+// profile related routes
 router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen)
 
-
-//post related routes
+// post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
-router.post('/create-post', userController.mustBeLoggedIn, postController.create )
+router.post('/create-post', userController.mustBeLoggedIn, postController.create)
 router.get('/post/:id', postController.viewSingle)
 router.get('/post/:id/edit', userController.mustBeLoggedIn, postController.viewEditScreen)
 router.post('/post/:id/edit', userController.mustBeLoggedIn, postController.edit)
-
-
-
-
 
 module.exports = router
